ba8a91d50cc7ca40170a426066936e4938d27567
s0lver <rperez.github@gmail.com>
2016-01-29 12:03:37 -0600

    First version of GUI for stay points comparison :octocat:

--- src/tamps/cinvestav/s0lver/spCalculator/FrmStayPointComparator.java
+++ src/tamps/cinvestav/s0lver/spCalculator/FrmStayPointComparator.java
@@ -2,22 +2,39 @@ package tamps.cinvestav.s0lver.spCalculator;
 
 import tamps.cinvestav.s0lver.iolocationfiles.readers.staypoints.StayPointsFileReader;
 import tamps.cinvestav.s0lver.locationentities.StayPoint;
+import tamps.cinvestav.s0lver.stayPointsComparator.comparator.StayPointsComparator;
+import tamps.cinvestav.s0lver.stayPointsComparator.comparatorResults.StayPointsComparatorResult;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
-public class FrmStayPointComparator extends JFrame {
+public class FrmStayPointComparator extends JFrame implements ActionListener{
 //    private ArrayList<StayPoint> stayPointsA;
 //    private ArrayList<StayPoint> stayPointsB;
 
     private final String DATE_FORMAT = "dd/MM/yyyy HH:mm:ss";
-    private JTable topTable;
+    private JTable topTable, bottomTable;
+    private JButton btnLoadFileA, btnLoadFileB;
+    private JButton btnBrowseForFileA, btnBrowseForFileB;
+    private JTextField txtSelectedFileA, txtSelectedFileB;
+    private JFileChooser fileChooser;
+    private File fileA, fileB;
+    private ArrayList<StayPoint> stayPointsA, stayPointsB;
+    JTextField txtArrivalTimeDifference,txtDepartureTimeDifference,txtStayTimeDifference, txtDistanceDifference;
+
+
     private final String[] COLUMN_NAMES = {
             "#",
             "Latitude",
@@ -30,7 +47,6 @@ public class FrmStayPointComparator extends JFrame {
 
     public FrmStayPointComparator() {
         super("Stay points comparator");
-        //setSize(800, 600);
 
         prepareGUIComponents();
         setVisible(true);
@@ -39,40 +55,100 @@ public class FrmStayPointComparator extends JFrame {
     }
 
     private void prepareGUIComponents() {
-        setLayout(new BorderLayout(3, 3));
-        JTable topTable = createTable();
-        JTable bottomTable = createTable();
-        ArrayList<StayPoint> stayPointsA = loadStayPointsTopTable();
-        ArrayList<StayPoint> stayPointsB = loadStayPointsBottomTable();
-
-        fillTable(topTable, stayPointsA);
-        fillTable(bottomTable, stayPointsB);
-
-        Dimension tablePreferredDimension = new Dimension(500, 150);
-        JScrollPane pnlTopTable = new JScrollPane(topTable);
-        pnlTopTable.setPreferredSize(tablePreferredDimension);
-
-        JScrollPane pnlBottomTable = new JScrollPane(bottomTable);
-        pnlBottomTable.setPreferredSize(tablePreferredDimension);
-
-        JPanel pnlTop = new JPanel();
-        pnlTop.setBorder(BorderFactory.createTitledBorder("First family of staypoints:"));
-
-        JPanel pnlBottom = new JPanel();
-        pnlBottom.setBorder(BorderFactory.createTitledBorder("Second family of staypoints:"));
+        fileChooser = new JFileChooser();
+        // setLayout(new BorderLayout(3, 3));
+        setLayout(new GridLayout(3, 1));
 
 
+        Dimension tablePreferredDimension = new Dimension(700, 150);
+
+        // Top table
+        topTable = createTable();
+        topTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                rowsSelectionChanged();
+            }
+        });
+        JScrollPane jspTopTable = new JScrollPane(topTable);
+        jspTopTable.setPreferredSize(tablePreferredDimension);
+        btnBrowseForFileA = new JButton("Browse");
+        btnBrowseForFileA.addActionListener(this);
+        btnLoadFileA = new JButton("Load");
+        btnLoadFileA.addActionListener(this);
+        txtSelectedFileA = new JTextField("Select a file!", 40);
+        txtSelectedFileA.setEditable(false);
+        JPanel topTablePanel = createInputComponent(jspTopTable, btnBrowseForFileA, btnLoadFileA, txtSelectedFileA);
+
+        // Bottom table
+        bottomTable = createTable();
+        bottomTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                rowsSelectionChanged();
+            }
+        });
+        JScrollPane jspBottomTable = new JScrollPane(bottomTable);
+        jspBottomTable.setPreferredSize(tablePreferredDimension);
+        btnBrowseForFileB = new JButton("Browse");
+        btnBrowseForFileB.addActionListener(this);
+        btnLoadFileB = new JButton("Load");
+        btnLoadFileB.addActionListener(this);
+        txtSelectedFileB = new JTextField("Select a file!", 40);
+        txtSelectedFileB.setEditable(false);
+        JPanel bottomTablePanel = createInputComponent(jspBottomTable, btnBrowseForFileB, btnLoadFileB, txtSelectedFileB);
+
+        // Results
         JPanel pnlResults = new JPanel();
-        pnlResults.setBorder(BorderFactory.createTitledBorder("Results of comparison"));
+        pnlResults.setLayout(new GridLayout(4,2));
+        pnlResults.setBorder(BorderFactory.createTitledBorder("Comparison:"));
         pnlResults.setPreferredSize(tablePreferredDimension);
+        JLabel lblArrivalTimeDifference = new JLabel("Arrival time: ");
+        JLabel lblDepartureTimeDifference = new JLabel("Departure time: ");
+        JLabel lblStayTimeDifference = new JLabel("Stay time: ");
+        JLabel lblDistanceDifference = new JLabel("Distance: ");
 
-        pnlTop.add(pnlTopTable);
-        pnlBottom.add(pnlBottomTable);
+        txtArrivalTimeDifference = new JTextField("Select two stay points!");
+        txtDepartureTimeDifference = new JTextField("Select two stay points!");
+        txtStayTimeDifference = new JTextField("Select two stay points!");
+        txtDistanceDifference = new JTextField("Select two stay points!");
+        txtDepartureTimeDifference.setEditable(false);
+        txtArrivalTimeDifference.setEditable(false);
+        txtStayTimeDifference.setEditable(false);
+        txtDistanceDifference.setEditable(false);
+
+        pnlResults.add(lblArrivalTimeDifference);
+        pnlResults.add(txtArrivalTimeDifference);
+        pnlResults.add(lblDepartureTimeDifference);
+        pnlResults.add(txtDepartureTimeDifference);
+        pnlResults.add(lblStayTimeDifference);
+        pnlResults.add(txtStayTimeDifference);
+        pnlResults.add(lblDistanceDifference);
+        pnlResults.add(txtDistanceDifference);
 
 
-        add(pnlTop, BorderLayout.NORTH);
-        add(pnlBottom, BorderLayout.CENTER);
-        add(pnlResults, BorderLayout.SOUTH);
+        //add(topTablePanel, BorderLayout.NORTH);
+        //add(bottomTablePanel, BorderLayout.CENTER);
+        //add(pnlResults, BorderLayout.SOUTH);
+        add(topTablePanel);
+        add(bottomTablePanel);
+        add(pnlResults);
+    }
+
+    private JPanel createInputComponent(JScrollPane pnlTable, JButton btnBrowse, JButton btnLoad, JTextField txtSelectedFile) {
+        JPanel innerTopPanel = new JPanel();
+        innerTopPanel.add(new JLabel("File:"));
+        innerTopPanel.add(txtSelectedFile);
+        innerTopPanel.add(btnBrowse);
+        innerTopPanel.add(btnLoad);
+
+        JPanel completePanel = new JPanel();
+        completePanel.setBorder(BorderFactory.createTitledBorder("Stay points origin:"));
+        completePanel.setLayout(new BoxLayout(completePanel, BoxLayout.Y_AXIS));
+        completePanel.add(innerTopPanel);
+        completePanel.add(pnlTable);
+
+        return completePanel;
     }
 
     private JTable createTable() {
@@ -87,6 +163,11 @@ public class FrmStayPointComparator extends JFrame {
         table.setColumnSelectionAllowed(false);
         TableColumn column = table.getColumnModel().getColumn(0);
         column.setPreferredWidth(1);
+
+        column = table.getColumnModel().getColumn(3);
+        column.setPreferredWidth(100);
+        column = table.getColumnModel().getColumn(4);
+        column.setPreferredWidth(100);
 
         table.setFillsViewportHeight(true);
 
@@ -105,18 +186,6 @@ public class FrmStayPointComparator extends JFrame {
         }
     }
 
-    private ArrayList<StayPoint> loadStayPointsTopTable() {
-        String pathStayPointsFile = "C:\\Users\\rafael\\desktop\\tmp\\exp\\sp-1\\staypoints.csv";
-        StayPointsFileReader stayPointsFileReader = new StayPointsFileReader(pathStayPointsFile);
-        return stayPointsFileReader.readFile();
-    }
-
-    private ArrayList<StayPoint> loadStayPointsBottomTable() {
-        String pathStayPointsFile = "C:\\Users\\rafael\\desktop\\tmp\\exp\\sp-2\\staypoints.csv";
-        StayPointsFileReader stayPointsFileReader = new StayPointsFileReader(pathStayPointsFile);
-        return stayPointsFileReader.readFile();
-    }
-
     private Object[] chunkStayPoint(StayPoint stayPoint) {
         SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH);
         long timeDifferenceMs = stayPoint.getDepartureTime().getTime() - stayPoint.getArrivalTime().getTime();
@@ -131,5 +200,78 @@ public class FrmStayPointComparator extends JFrame {
                 timeDifferenceMinutes + " min",
                 stayPoint.getAmountFixes()
         };
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnBrowseForFileA){
+            int returnVal = fileChooser.showOpenDialog(FrmStayPointComparator.this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+                this.fileA = selectedFile;
+                this.txtSelectedFileA.setText(selectedFile.getAbsolutePath());
+            }
+        }
+        else if (e.getSource() == btnBrowseForFileB){
+            int returnVal = fileChooser.showOpenDialog(FrmStayPointComparator.this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+                this.fileB = selectedFile;
+                this.txtSelectedFileB.setText(selectedFile.getAbsolutePath());
+            }
+        }
+        else if (e.getSource() == btnLoadFileA){
+            stayPointsA = readFromFile(fileA);
+            fillTable(topTable, stayPointsA);
+        }
+        else if (e.getSource() == btnLoadFileB){
+            stayPointsB = readFromFile(fileB);
+            fillTable(bottomTable, stayPointsB);
+        }
+    }
+
+    public void rowsSelectionChanged(){
+        int selectedRowTT = topTable.getSelectedRow();
+        int selectedRowBT = bottomTable.getSelectedRow();
+
+        if (selectedRowTT!=-1 &&selectedRowBT != -1 ){
+            StayPoint stayPointA = stayPointsA.get(selectedRowTT);
+            StayPoint stayPointB = stayPointsB.get(selectedRowBT);
+            StayPointsComparator stayPointsComparator = new StayPointsComparator(stayPointA, stayPointB);
+            StayPointsComparatorResult stayPointsComparatorResult = stayPointsComparator.compareStayPoints();
+            showComparisonResults(stayPointsComparatorResult);
+        }
+    }
+
+    private void showComparisonResults(StayPointsComparatorResult stayPointsComparatorResult) {
+        String arrivalTimeDifference = getHumanTimeData(stayPointsComparatorResult.getArrivalTimeDifference());
+        String departureTimeDifference = getHumanTimeData(stayPointsComparatorResult.getDepartureTimeDifference());
+        String stayTimeDifference = getHumanTimeData(stayPointsComparatorResult.getStayTimeDifference());
+        String distanceDifference = String.valueOf(stayPointsComparatorResult.getDistanceDifference()) + " meters";
+
+        txtArrivalTimeDifference.setText(arrivalTimeDifference);
+        txtDepartureTimeDifference.setText(departureTimeDifference);
+        txtStayTimeDifference.setText(stayTimeDifference);
+        txtDistanceDifference.setText(distanceDifference);
+    }
+
+    private String getHumanTimeData(long timeDifference) {
+        StringBuilder sb = new StringBuilder();
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(timeDifference);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(timeDifference);
+        long hours = TimeUnit.MILLISECONDS.toHours(timeDifference);
+        sb.append(String.valueOf(seconds))
+                .append(" sec, ")
+                .append(String.valueOf(minutes))
+                .append(" mins, ")
+                .append(String.valueOf(hours))
+                .append(" hrs");
+
+        return sb.toString();
+    }
+
+    private ArrayList<StayPoint> readFromFile(File file) {
+        StayPointsFileReader sfr = new StayPointsFileReader(file.getAbsolutePath());
+        return sfr.readFile();
     }
 }
