fb47182573162fefc1b89909921f78e080a851f6
s0lver <rperez.github@gmail.com>
2016-04-13 18:54:17 -0500

    Advances on debugging

--- TrajectoriesComparator/src/tamps/cinvestav/s0lver/trajectoriescomparator/TrajectoryComparator.java
+++ TrajectoriesComparator/src/tamps/cinvestav/s0lver/trajectoriescomparator/TrajectoryComparator.java
@@ -77,93 +77,52 @@ public class TrajectoryComparator {
         return distanceSum;
     }
 
-    private ArrayList<GpsFix> mapInternalFixesEuclideanly(Trajectory subTrajectory, GpsFix leftFix, GpsFix rightFix) {
-        ArrayList<GpsFix> mappedFixes = new ArrayList<>();
-
-        // Map only internal fixes
-        int sizeSubTrajectory = subTrajectory.getSize();
-        for (int currentMappedFixIndex = 1; currentMappedFixIndex < (sizeSubTrajectory - 1); currentMappedFixIndex++) {
-            double k1 = currentMappedFixIndex;
-            double k2 = sizeSubTrajectory - k1;
-            GpsFix mappedFix = buildSyntheticFix(currentMappedFixIndex, leftFix, rightFix, k1, k2);
-            mappedFixes.add(mappedFix);
-        }
-
-        return mappedFixes;
-    }
-
-    public double compareSynchronized() {
-        double distanceSum = 0;
-        for (int i = 0; i < subSampledTrajectory.getSize(); i++) {
-            GpsFix sampledFix = subSampledTrajectory.getFix(i);
-            int indexEquivalentFixLeft = i;
-
-            // If we are @ last sampled fix, then everything is already done
-            if (i + 1 == subSampledTrajectory.getSize()) {
-                break;
-            }
-
-            int indexEquivalentFixRight = groundTruthTrajectory.getTimeClosestFixIndex(subSampledTrajectory.getFix(i + 1), indexEquivalentFixLeft);
-            Trajectory subTrajectory = groundTruthTrajectory.getSubTrajectory(indexEquivalentFixLeft, indexEquivalentFixRight);
-
-            for (int indexCurrentMappedFix = 1; indexCurrentMappedFix < subTrajectory.getSize(); indexCurrentMappedFix++) {
-                GpsFix groundTruthFix = subTrajectory.getFix(indexCurrentMappedFix);
-                GpsFix syntheticFix = mapFixSynchronized(subTrajectory, indexCurrentMappedFix, indexEquivalentFixLeft, indexEquivalentFixRight);
-                distanceSum += groundTruthFix.distanceTo(syntheticFix);
-            }
-        }
-        return distanceSum;
-    }
-
-    private GpsFix mapFixSynchronized(Trajectory subTrajectory, int currentMappedFix, int indexLeftFix, int indexRightFix) {
-        int subTrajectorySize = subTrajectory.getSize();
-
-        double gtInternalDistance = subTrajectory.getInternalDistance();
-        double accumulatedDistanceToCurrentFix = subTrajectory.getSubTrajectory(0, currentMappedFix).getInternalDistance();
-
-        GpsFix ssLeftFix = subSampledTrajectory.getFix(indexLeftFix);
-        GpsFix ssRightFix = subSampledTrajectory.getFix(indexRightFix);
-
-        double k1 = accumulatedDistanceToCurrentFix;
-        double k2 = gtInternalDistance - k1;
-
-        GpsFix mappedGpsFix = buildSyntheticFix(currentMappedFix, ssLeftFix, ssRightFix, k1, k2);
-
-        return mappedGpsFix;
-    }
-
-    private GpsFix mapFixEuclidean(Trajectory subTrajectory, int currentMappedFix, int indexLeftFix, int indexRightFix) {
-        int subTrajectorySize = subTrajectory.getSize();
-
-        GpsFix ssLeftFix = subSampledTrajectory.getFix(indexLeftFix);
-        GpsFix ssRightFix = subSampledTrajectory.getFix(indexRightFix);
-
-        // Although based on time, here k1 and k2 are expressing only geometric aspects
-        int k1 = currentMappedFix;
-        int k2 = subTrajectorySize - k1;
-        // we know that subTrajectorySize = k1 + k2
-        GpsFix mappedGpsFix = buildSyntheticFix(currentMappedFix, ssLeftFix, ssRightFix, k1, k2);
-
-        return mappedGpsFix;
-    }
-
-    private GpsFix buildSyntheticFix(int currentMappedFix, GpsFix ssInitialFix, GpsFix ssLastFix, double k1, double k2) {
-        double mappedLatitude = ((k1 * ssLastFix.getLatitude()) + (k2 * ssInitialFix.getLatitude())) / (k1 + k2);
-        double mappedLongitude = ((k1 * ssLastFix.getLongitude()) + (k2 * ssInitialFix.getLongitude())) / (k1 + k2);
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(ssInitialFix.getTimestamp());
-        calendar.add(Calendar.SECOND, currentMappedFix);
-
-        return new GpsFix(mappedLatitude, mappedLongitude, 0, 0, 0, calendar.getTime());
-    }
-
+//    public double compareSynchronized() {
+//        double distanceSum = 0;
+//        for (int i = 0; i < subSampledTrajectory.getSize(); i++) {
+//            GpsFix sampledFix = subSampledTrajectory.getFix(i);
+//            int indexEquivalentFixLeft = i;
+//
+//            // If we are @ last sampled fix, then everything is already done
+//            if (i + 1 == subSampledTrajectory.getSize()) {
+//                break;
+//            }
+//
+//            int indexEquivalentFixRight = groundTruthTrajectory.getTimeClosestFixIndex(subSampledTrajectory.getFix(i + 1), indexEquivalentFixLeft);
+//            Trajectory subTrajectory = groundTruthTrajectory.getSubTrajectory(indexEquivalentFixLeft, indexEquivalentFixRight);
+//
+//            for (int indexCurrentMappedFix = 1; indexCurrentMappedFix < subTrajectory.getSize(); indexCurrentMappedFix++) {
+//                GpsFix groundTruthFix = subTrajectory.getFix(indexCurrentMappedFix);
+//                GpsFix syntheticFix = mapFixSynchronized(subTrajectory, indexCurrentMappedFix, indexEquivalentFixLeft, indexEquivalentFixRight);
+//                distanceSum += groundTruthFix.distanceTo(syntheticFix);
+//            }
+//        }
+//        return distanceSum;
+//    }
+//
+//    private GpsFix mapFixSynchronized(Trajectory subTrajectory, int currentMappedFix, int indexLeftFix, int indexRightFix) {
+//        int subTrajectorySize = subTrajectory.getSize();
+//
+//        double gtInternalDistance = subTrajectory.getInternalDistance();
+//        double accumulatedDistanceToCurrentFix = subTrajectory.getSubTrajectory(0, currentMappedFix).getInternalDistance();
+//
+//        GpsFix ssLeftFix = subSampledTrajectory.getFix(indexLeftFix);
+//        GpsFix ssRightFix = subSampledTrajectory.getFix(indexRightFix);
+//
+//        double k1 = accumulatedDistanceToCurrentFix;
+//        double k2 = gtInternalDistance - k1;
+//
+//        GpsFix mappedGpsFix = buildSyntheticFix(currentMappedFix, ssLeftFix, ssRightFix, k1, k2);
+//
+//        return mappedGpsFix;
+//    }
 
     /***
      * Measures the distance between two GPS trajectories
      * @see Trajectory
      * @return The distance between the two trajectories, converted to meters.
      */
-    public ArrayList<GpsFix> getSyntheticFixes() {
+    public ArrayList<GpsFix> getSyntheticFixesEuclideanly() {
         ArrayList<GpsFix> internalMappedFixes = new ArrayList<>();
         double distanceSum = 0;
         int subSampledTrajectorySize = subSampledTrajectory.getSize();
@@ -191,5 +150,76 @@ public class TrajectoryComparator {
         allFixes.addAll(internalMappedFixes);
         allFixes.add(rightFix);
         return allFixes;
+    }
+
+    private ArrayList<GpsFix> mapInternalFixesEuclideanly(Trajectory subTrajectory, GpsFix leftFix, GpsFix rightFix) {
+        ArrayList<GpsFix> mappedFixes = new ArrayList<>();
+
+        // Map only internal fixes
+        int sizeSubTrajectory = subTrajectory.getSize();
+        for (int currentMappedFixIndex = 1; currentMappedFixIndex < (sizeSubTrajectory - 1); currentMappedFixIndex++) {
+            double k1 = currentMappedFixIndex;
+            double k2 = sizeSubTrajectory - k1;
+            GpsFix mappedFix = buildSyntheticFix(currentMappedFixIndex, leftFix, rightFix, k1, k2);
+            mappedFixes.add(mappedFix);
+        }
+
+        return mappedFixes;
+    }
+
+
+    public ArrayList<GpsFix> getSyntheticFixesSynchronizedly() {
+        ArrayList<GpsFix> internalMappedFixes = new ArrayList<>();
+        double distanceSum = 0;
+        int subSampledTrajectorySize = subSampledTrajectory.getSize();
+        int indexEquivalentFixLeft = 0, indexEquivalentFixRight = 0;
+        int i = 0;
+
+        // Get anchor points in subsampled trajectory
+        GpsFix leftFix = subSampledTrajectory.getFix(i);
+        GpsFix rightFix = subSampledTrajectory.getFix(i + 1);
+
+        indexEquivalentFixLeft = groundTruthTrajectory.getTimeClosestFixIndex(subSampledTrajectory.getFix(i), indexEquivalentFixLeft);
+        indexEquivalentFixRight = groundTruthTrajectory.getTimeClosestFixIndex(subSampledTrajectory.getFix(i + 1), indexEquivalentFixLeft);
+        Trajectory subTrajectory = groundTruthTrajectory.getSubTrajectory(indexEquivalentFixLeft, indexEquivalentFixRight);
+
+        double distanceFirstPoint = subTrajectory.getFix(0).distanceTo(leftFix);
+        distanceSum += distanceFirstPoint;
+
+        internalMappedFixes = mapInternalFixesSynchronizedly(subTrajectory, leftFix, rightFix);
+
+        ArrayList<GpsFix> allFixes = new ArrayList<>();
+        allFixes.add(leftFix);
+        allFixes.addAll(internalMappedFixes);
+        allFixes.add(rightFix);
+        return allFixes;
+    }
+
+    private ArrayList<GpsFix> mapInternalFixesSynchronizedly(Trajectory subTrajectory, GpsFix leftFix, GpsFix rightFix) {
+        ArrayList<GpsFix> mappedFixes = new ArrayList<>();
+        int sizeSubTrajectory = subTrajectory.getSize();
+
+        double groundTruthInternalDistance = subTrajectory.getInternalDistance();
+        for (int currentMappedFixIndex=1; currentMappedFixIndex < (sizeSubTrajectory -1); currentMappedFixIndex++) {
+            double accumulatedDistanceToCurrentFix = subTrajectory.getSubTrajectory(0, currentMappedFixIndex).getInternalDistance();
+
+            double k1 = accumulatedDistanceToCurrentFix;
+            double k2 = groundTruthInternalDistance - k1;
+
+            GpsFix mappedFix = buildSyntheticFix(currentMappedFixIndex, leftFix, rightFix, k1, k2);
+            mappedFixes.add(mappedFix);
+        }
+
+        return mappedFixes;
+    }
+
+    private GpsFix buildSyntheticFix(int currentMappedFix, GpsFix ssInitialFix, GpsFix ssLastFix, double k1, double k2) {
+        double mappedLatitude = ((k1 * ssLastFix.getLatitude()) + (k2 * ssInitialFix.getLatitude())) / (k1 + k2);
+        double mappedLongitude = ((k1 * ssLastFix.getLongitude()) + (k2 * ssInitialFix.getLongitude())) / (k1 + k2);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(ssInitialFix.getTimestamp());
+        calendar.add(Calendar.SECOND, currentMappedFix);
+
+        return new GpsFix(mappedLatitude, mappedLongitude, 0, 0, 0, calendar.getTime());
     }
 }
--- src/tamps/cinvestav/s0lver/spCalculator/Main.java
+++ src/tamps/cinvestav/s0lver/spCalculator/Main.java
@@ -1,21 +1,11 @@
 package tamps.cinvestav.s0lver.spCalculator;
 
-import tamps.cinvestav.s0lver.iolocationfiles.readers.gpsFixes.GPSFixesFileReader;
 import tamps.cinvestav.s0lver.iolocationfiles.readers.gpsFixes.LoggerReaderFixes;
 import tamps.cinvestav.s0lver.iolocationfiles.readers.gpsFixes.SmartphoneFixesFileReader;
-import tamps.cinvestav.s0lver.iolocationfiles.readers.staypoints.StayPointsFileReader;
 import tamps.cinvestav.s0lver.iolocationfiles.writers.GpsFixCsvWriter;
-import tamps.cinvestav.s0lver.iolocationfiles.writers.StayPointCsvWriter;
-import tamps.cinvestav.s0lver.kmltranslator.translators.KmlFileCreator;
 import tamps.cinvestav.s0lver.kmltranslator.translators.PinnedKmlCreator;
 import tamps.cinvestav.s0lver.locationentities.GpsFix;
-import tamps.cinvestav.s0lver.locationentities.StayPoint;
 import tamps.cinvestav.s0lver.locationentities.Trajectory;
-import tamps.cinvestav.s0lver.stayPointsCalculator.algorithms.live.buffered.MontoliouBufferedAlgorithm;
-import tamps.cinvestav.s0lver.stayPointsCalculator.algorithms.offline.MontoliuAlgorithm;
-import tamps.cinvestav.s0lver.stayPointsCalculator.algorithms.offline.OfflineAlgorithm;
-import tamps.cinvestav.s0lver.stayPointsCalculator.algorithms.offline.ZhenAlgorithm;
-import tamps.cinvestav.s0lver.stayPointsCalculator.gui.FrmMain;
 import tamps.cinvestav.s0lver.trajectoriescomparator.TrajectoryComparator;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -28,29 +18,7 @@ public class Main {
     public static final int ONE_MINUTE = 60 * 1000;
 
     public static void main(String[] args) throws IOException, ParseException, TransformerException, ParserConfigurationException {
-//        String gpsFixesSmartphoneOne = "C:\\Users\\rafael\\desktop\\tmp\\exp\\sp-1\\registros.csv";
-//        SmartphoneFixesFileReader sfr1 = new SmartphoneFixesFileReader(gpsFixesSmartphoneOne);
-//        ArrayList<GpsFix> gpsFixesSmartphone1 = sfr1.readFile();
-//
-//        String gpsFixesSmartphoneTwo = "C:\\Users\\rafael\\desktop\\tmp\\exp\\sp-2\\registros.csv";
-//        SmartphoneFixesFileReader sfr2 = new SmartphoneFixesFileReader(gpsFixesSmartphoneTwo);
-//        ArrayList<GpsFix> gpsFixesSmartphone2 = sfr2.readFile();
-//
-//        String gpsFixesLogger = "c:\\Users\\rafael\\Desktop\\tmp\\exp\\logger\\exported-20_28-10-2015.csv";
-//        LoggerReaderFixes loggerReaderFixes = new LoggerReaderFixes(gpsFixesLogger);
-//        ArrayList<GpsFix> gpsFixesFromLogger = loggerReaderFixes.readFile();
-//
-//        processSmartphoneOne(gpsFixesSmartphone1);
-//        processSmartphoneTwo(gpsFixesSmartphone2);
-//        processGpsLogger(gpsFixesFromLogger);
-
 //         new FrmMain();
-//        StayPointsFileReader reader = new StayPointsFileReader("C:\\Users\\Rafael\\Desktop\\staypoints_sigma-zhen_fixed-period_03-Mar-2016-16-21.csv");
-//        ArrayList<StayPoint> stayPoints = reader.readFile();
-//
-//        PinnedKmlCreator kmlCreator = PinnedKmlCreator.createForStayPoints("C:\\Users\\Rafael\\Desktop\\staypoints_sigma-zhen_fixed-period_03-Mar-2016-16-21.kml", stayPoints);
-//        kmlCreator.create();
-
         String groundTruthPath = "c:\\Users\\rafael\\Desktop\\tmp\\trajectories\\victoria-guemez.csv";
         LoggerReaderFixes groundTruthReader = new LoggerReaderFixes(groundTruthPath);
         ArrayList<GpsFix> groundTruthFixes = groundTruthReader.readFile();
@@ -67,16 +35,33 @@ public class Main {
 //        ArrayList<GpsFix> synthetic = comparator.compareEuclidean();
 //        PinnedKmlCreator.createForGpsFixes("C:\\Users\\rafael\\Desktop\\tmp\\trajectories\\synthetic-fixes.kml", synthetic).create();
 //        PinnedKmlCreator.createForGpsFixes("C:\\Users\\rafael\\Desktop\\tmp\\trajectories\\originalFixes.kml", groundTruthFixes).create();
-//        ArrayList<GpsFix> syntheticFixes = comparator.getSyntheticFixes();
+//        ArrayList<GpsFix> syntheticFixes = comparator.getSyntheticFixesEuclideanly();
 //        GpsFixCsvWriter writer = new GpsFixCsvWriter("c:\\Users\\rafael\\Desktop\\tmp\\trajectories\\synthetic-fixes.csv", syntheticFixes);
 //        writer.writeFile();
 
 
-        String syntheticFixesPath = "c:\\Users\\rafael\\Desktop\\tmp\\trajectories\\synthetic-fixes.csv";
-        SmartphoneFixesFileReader reader = new SmartphoneFixesFileReader(syntheticFixesPath);
-        ArrayList<GpsFix> syntheticFixes = reader.readFile();
-    // TODO something is wrong with the calculateEuclideanly, see that below the result is more believable
+        String syntheticFixedEuclideanPath = "c:\\Users\\rafael\\Desktop\\tmp\\trajectories\\synthetic-fixes-euclid.csv";
+        SmartphoneFixesFileReader readerEuclid = new SmartphoneFixesFileReader(syntheticFixedEuclideanPath);
+        ArrayList<GpsFix> syntheticEuclidFixes = readerEuclid.readFile();
+        double distanceSum = calculateDistance(groundTruthFixes, syntheticEuclidFixes);
+        System.out.println("Sum of distances is " + distanceSum);
 
+        String syntheticFixesSyncPath = "c:\\Users\\rafael\\Desktop\\tmp\\trajectories\\synthetic-fixes-synch.csv";
+        SmartphoneFixesFileReader readerSync = new SmartphoneFixesFileReader(syntheticFixesSyncPath);
+        ArrayList<GpsFix> syntheticSyncFixes = readerSync.readFile();
+        double distanceSumSync = calculateDistance(groundTruthFixes, syntheticSyncFixes);
+        System.out.println("Sum of distances is " + distanceSumSync);
+
+//        ArrayList<GpsFix> syntheticFixes = comparator.getSyntheticFixesSynchronizedly();
+//        GpsFixCsvWriter writer = new GpsFixCsvWriter("c:\\Users\\rafael\\Desktop\\tmp\\trajectories\\synthetic-fixes-synch.csv", syntheticFixes);
+//        writer.writeFile();
+//        PinnedKmlCreator.createForGpsFixes("C:\\Users\\rafael\\Desktop\\tmp\\trajectories\\synthetic-fixes-synch.kml", syntheticFixes).create();
+//        PinnedKmlCreator.createForGpsFixes("C:\\Users\\rafael\\Desktop\\tmp\\trajectories\\original-fixes.kml",
+//                getSubArrayList(groundTruthFixes, syntheticFixes.size())).create();
+
+    }
+
+    private static double calculateDistance(ArrayList<GpsFix> groundTruthFixes, ArrayList<GpsFix> syntheticFixes) {
         double distanceSum = 0;
         for (int i = 0; i < syntheticFixes.size(); i++) {
             GpsFix syntheticFix = syntheticFixes.get(i);
@@ -85,6 +70,14 @@ public class Main {
             System.out.println(syntheticFix + " ->VS<- " + groundTruthFix + "\t = " + distance);
             distanceSum += distance;
         }
-        System.out.println("Sum of distances is " + distanceSum);
+        return distanceSum;
+    }
+
+    private static ArrayList<GpsFix> getSubArrayList(ArrayList<GpsFix> original, int amount) {
+        ArrayList<GpsFix> newArrayList = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            newArrayList.add(original.get(i));
+        }
+        return newArrayList;
     }
 }
